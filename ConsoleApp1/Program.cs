1fbebdeb4089a334a7bddc7bfa45507d68b4d330
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-28 14:38:53 -0500

    great code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿{using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My code doesn't work I don't know why.  My code works, I don't know why");
+            Console.WriteLine("Lrn2Kode");
+            Console.WriteLine("Hello World! I am in ");
+            //Hi Nick, notice me pls
+
+            Console.WriteLine("Nick Enscals, Nick Enscala")
+                 //Self Driving Car
+        if (GoingToHitStuff == true)
+            {
+                Dont();
+                var a = new { 1, 2, 3, 4, 5 };
+            }// nothing to see heree.....
+        new
+        //Does this compile yet??? I need you guys to push pls I cant compile
+        //CAN I HAVE 0.5 POINTS IN MY QUIZ PLS, WILL PAY IN COFFEE
+        
+
+INO
+
+                /*
+
+               ___  _____    
+ .'/,-Y"     "~-.  
+ l.Y             ^.           
+ /\               _\_      "Doh!"   
+i            ___/"   "\ 
+|          /"   "\   o !   
+l         ]     o !__./   
+ \ _  _    \.___./    "~\  
+  X \/ \            ___./  
+ ( \ ___.   _..--~~"   ~`-.  
+  ` Z,--   /               \    
+    \__.  (   /       ______) 
+      \   l  /-----~~" /      -Row
+       Y   \          / 
+       |    "x______.^ 
+       |           \    
+       j            Y
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        public static void doStuff()
+        {
+            Console.WriteLine("
+"                   ___  _____"
+ .'/,-Y"     "~-.  
+ l.Y ^.
+ /\               _\_      "Doh!"
+i            ___ / "   "\ 
+|          / "   "\   o!
+l         ]     o!__./
+ \ _ _    \.___./ "~\  
+  X \/ \            ___./
+ ( \ ___._..--~~"   ~`-.  
+  ` Z,--   /               \    
+    \__.  (   / ______)
+      \   l / -----~~" /      -Row
+       Y   \          /
+       | "x______.^ 
+       |           \    
+       j Y")''
+    }
+}
